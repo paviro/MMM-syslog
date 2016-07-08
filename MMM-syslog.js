d4b55b9168b9ad50ce1b2cf059202b28613056e4
Paul-Vincent Roll <paviro@me.com>
2016-07-08 15:04:39 +0200

    Added DOM display.

--- MMM-syslog.js
+++ MMM-syslog.js
@@ -11,10 +11,40 @@ Module.register('MMM-syslog',{
 	start: function() {
 		this.sendSocketNotification("CONNECT");
 		Log.info("Starting module: " + this.name);
+		
+		this.messages = []
 	 },
 	
 	socketNotificationReceived: function(notification, payload) {
 		this.sendNotification("SHOW_ALERT", {type: "notification", title: payload.title, message: payload.message}); 
+		this.messages.push({title: payload.title, message: payload.message});
+		this.updateDom(3000);
 	 },
+	
+	getDom: function() {
+		var array = this.messages;
+
+		var arrayLength = array.length;
+		var theTable = document.createElement('table');
+		var wrapper = document.createElement("div");
+
+		// Note, don't forget the var keyword!
+		for (var i = 0, tr, td; i < arrayLength; i++) {
+			tr = document.createElement('tr');
+			td = document.createElement('td');
+			td.appendChild(document.createTextNode(array[i].title + " - " + array[i].message));
+			tr.appendChild(td);
+			theTable.appendChild(tr);
+		}
+
+		wrapper.appendChild(theTable);
+		
+		
+		
+		wrapper.className = "normal small light";
+
+
+		return wrapper;
+	 }
 	
 });
